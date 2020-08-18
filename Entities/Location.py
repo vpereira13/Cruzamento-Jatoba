7e7544b1a4cefcbe543d8d88b8bd77f6ea1892e8
vpereira <vpereira@protonmail.com>
2020-08-17 22:04:52 -0300

    Adiciona entidade de localização

---
+++ Entities/Location.py
@@ -0,0 +1,15 @@
+class Location:
+    def __init__(self):
+        pass
+
+    def set_x(self, x: float):
+        self.__x = x
+
+    def set_y(self, y: float):
+        self.__y = y
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
