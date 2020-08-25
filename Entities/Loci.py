06bdc5a3c0e75143133b34ec8b737d3d5108ddc9
vpereira <vpereira@protonmail.com>
2020-08-25 17:38:32 -0300

    Adição de representação de Loci como string

--- Entities/Loci.py
+++ Entities/Loci.py
@@ -14,3 +14,15 @@ class Loci:
 
     def all(self):
         return self.__collector
+
+    def __str__(self):
+        return "\t{}\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}".format(
+            str(self.__collector[0]),
+            str(self.__collector[1]),
+            str(self.__collector[2]),
+            str(self.__collector[3]),
+            str(self.__collector[4]),
+            str(self.__collector[5]),
+            str(self.__collector[6]),
+            str(self.__collector[7]),
+        )
