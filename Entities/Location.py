e33f24a4c5ac6a198f22c4a185dbc545fe90e77e
vpereira <vpereira@protonmail.com>
2020-08-25 17:35:25 -0300

    Modificação to tipo variáveis de localização

--- Entities/Location.py
+++ Entities/Location.py
@@ -2,10 +2,10 @@ class Location:
     def __init__(self):
         pass
 
-    def set_x(self, x: float):
+    def set_x(self, x: int):
         self.__x = x
 
-    def set_y(self, y: float):
+    def set_y(self, y: int):
         self.__y = y
 
     def get_x(self):
