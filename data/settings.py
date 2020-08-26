96ab95d7b8a30ef9dd8c9ab371538e74a24b5897
vpereira <vpereira@protonmail.com>
2020-08-25 22:48:19 -0300

    Adição do arquivo de configurações, env vars

---
+++ data/settings.py
@@ -0,0 +1,19 @@
+# Simulation envs
+YEAR_MAX = 50
+MATCHES = 200
+SELF_FECUNDATION_RATE = 27
+
+# Death rate envs
+ZERO_AGE_DEATH_RATE = 980
+YOUNG_DEATH_RATE = 460
+ADULT_DEATH_RATE = 100
+
+# Files envs
+INPUT_FILENAME = 'example.in'
+CHECKPOINT_PERIODICITY = 5
+
+# Location envs
+Xi = 30
+Xf = 30000
+Yi = 30
+Yf = 30000
